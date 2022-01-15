dbfa4d37156402f6e12ffd6e2525ba547b7ce0a1
unknown <abdallah.algamal93@gmail.com>
2022-01-15 21:26:03 +0400

    Sixth commit

--- src/components/SectionFive.vue
+++ src/components/SectionFive.vue
@@ -1,13 +1,29 @@
 <template>
     <div :style="sectionBackgroundImage" class="container-fluid">
-        <div class="row">
+        <div class="row newspaper-section">
             <div class="col-4 newspaper-soldier-section">
                 <img src="newspaperSoldier.png">
             </div>
             <div class="col-8 newspaper-soldier-form-section">
                 <img src="Heading.png">
                 <p>Get the latest updates about our new games and prizes</p>
-                <input type="email" class="form-control newspaper-soldier-form-mail-input" id="inputEmail" placeholder="Enter your email">      
+                <input type="email" class="form-control newspaper-soldier-form-mail-input" id="inputEmail" placeholder="Enter your email">  
+                <button class="subscribe-btn">S</button>    
+            </div>
+        </div>
+        <div class="row footer-section">
+            <div class="col-4">
+                <p class="copyright-par">©2022 <span class="purple-color">365games.</span> All Rights Reserved.</p>
+            </div>
+            <div class="col-4">
+                <a class="footer-links">Advertise</a>
+                <a class="footer-links">Sponsor</a>
+                <a class="footer-links">Careers</a>
+            </div>
+            <div class="col-4">
+                <a class="social-media-icons"><img src="Facebook.png"></a>
+                <a class="social-media-icons"><img src="Twitter.png"></a>
+                <a class="social-media-icons"><img src="Instagram.png"></a>
             </div>
         </div>
     </div>
@@ -30,6 +46,10 @@ export default {
     background-color: black;
     background-repeat: no-repeat;
     color: #ffffff;
+}
+
+.newspaper-section {
+    padding-bottom: 100px;
 }
 
 .newspaper-soldier-section {
@@ -60,6 +80,7 @@ export default {
 }
 
 .newspaper-soldier-form-mail-input {
+    display: inline-block;
     width: 330.58px !important;
     height: 65.55px !important;
     background-color: rgba(255, 255, 255, 0.08) !important;
@@ -68,7 +89,49 @@ export default {
     color: #ffffff !important;
 }
 
+.newspaper-soldier-form-mail-input::placeholder {
+    color: #ffffff !important;
+}
+
 .newspaper-soldier-form-mail-input:focus {
     box-shadow: none !important;
+}
+
+.subscribe-btn {
+    
+}
+
+.footer-section {
+    text-align: center;
+    padding-bottom: 100px;
+}
+
+.copyright-par {
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    color: #FFFFFF;
+}
+
+.purple-color {
+    color: #9672FF;
+}
+
+.footer-links {
+    cursor: pointer;
+    font-family: 'Poppins';
+    font-style: normal;
+    font-weight: normal;
+    font-size: 16px;
+    text-decoration: none !important;
+    color: #FFFFFF;
+    margin-right: 30px;
+}
+
+.social-media-icons {
+    cursor: pointer;
+    text-decoration: none !important;
+    margin-right: 20px;
 }
 </style>
